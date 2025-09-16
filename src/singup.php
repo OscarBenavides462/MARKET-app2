5e85f2c88c7703366dfe6ffc61ba0f90deb4f2e9
Oscar b <159alejo753@gmail.com>
2025-09-16 15:35:39 -0500

    validacion riguster usre con front

--- src/singup.php
+++ src/singup.php
@@ -10,7 +10,20 @@ $e_mail = $_POST['email'];
 $p_wd = $_POST['passwd'];
 
 $enc_pass  =password_hash($p_wd, PASSWORD_DEFAULT);
-//step 3. create query to insert into
+
+$check_email = "select
+                    u.email
+                from 
+                users u
+                 where email='$e_mail' or $ide_number = 'id_number'
+                 limit 1  ";
+                 $res_check = pg_query($conn,$check_email);
+                 if(pg_num_rows($res_check)> 0)
+                    {
+                        echo "<script>alert('User already exist !!')</script>";
+                        header('refresh:0;url=singup.html');
+                    }else
+                    {
 $query="
 insert into users(
 firstname, lastname, mobile_number, 
@@ -27,8 +40,14 @@ $res = pg_query($conn, $query);
 //step 5. Validate result
 
 if($res){
-    echo "User has been created successfully !!!";
+    //echo "User has been created successfully !!!";
+    echo"<script>alert ('Success !!! Go to login')</script>";+
+    header('refresh:0;url=signin.html');
 }else{
-    echo "something wrong!";
+   echo "something wrong!";
 }
+                    }
+                 
+//step 3. create query to insert into
+
 ?>
