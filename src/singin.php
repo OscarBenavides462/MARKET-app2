4648373b955bb05c6bf871d86f4034bdfed72fa7
Oscar b <159alejo753@gmail.com>
2025-09-18 11:37:55 -0500

    create login request

---
+++ src/singin.php
@@ -0,0 +1,29 @@
+<?php     
+//step 1. Get database connection
+require('../config/database.php');
+//step 2. Get form-data
+
+$e_mail = $_POST['email'];
+$p_wd = $_POST['passwd'];
+
+//Step 3. Query to validate data
+$sql_check_user=" select 
+                    u.email,
+                    u.password
+                from
+                    users u
+                where
+                    u.email='$e_mail' and
+                    u.password = '$p_wd'
+                limit 1
+";
+//Step 4. Execute query
+$res_check = pg_query($conn, $sql_check_user);
+
+if(pg_num_rows($res_check) > 0){
+    echo "User exist. Go to main page!!";
+    
+}else{ 
+    echo "Verify data";
+}
+?>
--- src/singup.php
+++ src/singup.php
@@ -1,7 +1,7 @@
 <?php
 //step 1. Get database connection
 require('../config/database.php');
-//step 2. Get form-data
+ 
 $f_name = $_POST['fname'];
 $l_name = $_POST['lname'];
 $m_number = $_POST['mnumber'];
@@ -9,21 +9,23 @@ $id_number = $_POST['idnumber'];
 $e_mail = $_POST['email'];
 $p_wd = $_POST['passwd'];
 
-$enc_pass  =password_hash($p_wd, PASSWORD_DEFAULT);
+$ecn_pass = password_hash($p_wd, PASSWORD_DEFAULT );
+$check_email=" 
+SELECT 
+    u.email
+FROM
+    users u
+WHERE
+    email = '$e_mail' or ide_number = '$id_number'
+LIMIT 1    
+";
+$res_check = pg_query($conn, $check_email);
+if(pg_num_rows($res_check) > 0){
+    echo"<script>alert('User already exits !!')</script>";+
+    header('refresh:0;url=signup.html');
 
-$check_email = "select
-                    u.email
-                from 
-                users u
-                 where email='$e_mail' or $ide_number = 'id_number'
-                 limit 1  ";
-                 $res_check = pg_query($conn,$check_email);
-                 if(pg_num_rows($res_check)> 0)
-                    {
-                        echo "<script>alert('User already exist !!')</script>";
-                        header('refresh:0;url=singup.html');
-                    }else
-                    {
+} else{
+//step 3. create query to insert into
 $query="
 insert into users(
 firstname, lastname, mobile_number, 
@@ -31,7 +33,7 @@ ide_number, email, password
 ) 
 values(
 '$f_name', '$l_name', '$m_number', '$id_number', 
-'$e_mail', '$enc_pass'
+'$e_mail', '$p_wd'
 )
 ";
 //step 4. Execute query
@@ -41,13 +43,10 @@ $res = pg_query($conn, $query);
 
 if($res){
     //echo "User has been created successfully !!!";
-    echo"<script>alert ('Success !!! Go to login')</script>";+
+    echo"<script>alert('Success !!! Go to login')</script>";+
     header('refresh:0;url=signin.html');
 }else{
-   echo "something wrong!";
+    echo "something wrong!";
 }
-                    }
-                 
-//step 3. create query to insert into
+}
 
-?>
